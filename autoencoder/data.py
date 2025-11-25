d752c20aee74aa9c2ae9b77c760c6ab0a901188f
belam <belammuia0@gmail.com>
2025-11-25 22:32:43 +0300

    hot fixes

--- autoencoder/data.py
+++ autoencoder/data.py
@@ -16,28 +16,28 @@ def preprocess(data: pd.DataFrame, save_scaler=True):
 
     data.drop_duplicates(subset=['TxnID'], inplace=True)
     data = data.sort_values(['Msisdn', 'timestamp'])
-    # user behaviour features
-    data['user_avg_amount'] = data.groupby('Msisdn')['amount'].expanding().mean().reset_index(level=0, drop=True)
-    data['user_std_amount'] = data.groupby('Msisdn')['amount'].expanding().std().reset_index(level=0, drop=True)
-    data['amount_dev'] = data['amount'] - data['user_avg_amount']
+
+    data['user_avg_amount'] = data.groupby('Msisdn')['Amount'].expanding().mean().reset_index(level=0, drop=True)
+    data['user_std_amount'] = data.groupby('Msisdn')['Amount'].expanding().std().reset_index(level=0, drop=True)
+    data['amount_dev'] = data['Amount'] - data['user_avg_amount']
     
     data['txn_user_count'] = data.groupby('Msisdn').cumcount() + 1
     window_size = 5
-    data['txn_last_5'] = data.groupby('Msisdn')['amount'].rolling(window=window_size, min_periods=1).count().reset_index(level=0, drop=True)
+    data['txn_last_5'] = data.groupby('Msisdn')['Amount'].rolling(window=window_size, min_periods=1).count().reset_index(level=0, drop=True)
     
     data['time_since_last_txn'] = data.groupby('Msisdn')['timestamp'].diff().dt.total_seconds()
     data['time_since_last_txn'].fillna(999999, inplace=True)
-    data['velocity'] = data['amount'] / (data['time_since_last_txn'] + 1)
-    # Bill reference features
+    data['velocity'] = data['Amount'] / (data['time_since_last_txn'] + 1)
+
     data['txn_bill_ref_count'] = data.groupby('BillRefNumber')['TxnID'].transform('count')
     data['txn_bill_ref_unique'] = data.groupby('BillRefNumber')['Msisdn'].transform('nunique')
     data['is_success'] = (data['StatusCode'] == 0).astype(int)
     data['bill_success_rate'] = data.groupby('BillRefNumber')['is_success'].transform('mean')
-    #shortcode features
-    data['txn_shortcode_count'] = data.groupby('shortcode')['TxnID'].transform('count')
-    data['txn_shortcode_avg'] = data.groupby('shortcode')['amount'].transform('mean')
-    data['txn_shortcode_std'] = data.groupby('shortcode')['amount'].transform('std')
-    data['txn_success_ratio'] = data.groupby('shortcode')['is_success'].transform('mean')
+
+    data['txn_shortcode_count'] = data.groupby('ShortCode')['TxnID'].transform('count')
+    data['txn_shortcode_avg'] = data.groupby('ShortCode')['Amount'].transform('mean')
+    data['txn_shortcode_std'] = data.groupby('ShortCode')['Amount'].transform('std')
+    data['txn_success_ratio'] = data.groupby('ShortCode')['is_success'].transform('mean')
 
     onehot_cols = ['Response', 'StatusCode']
     data = pd.get_dummies(data, columns=onehot_cols, prefix=onehot_cols, drop_first=False)
